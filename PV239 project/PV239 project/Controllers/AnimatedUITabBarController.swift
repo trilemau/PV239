6de73b28250ea00a852e3e97ad3ea38261e00d73
Jan Crha <jancrha@gmail.com>
2019-03-27 18:06:07 +0100

    Add slide in/out animation when changing tabs

---
+++ PV239 project/PV239 project/Controllers/AnimatedUITabBarController.swift
@@ -0,0 +1,85 @@
+//
+//  AnimatedUITabBarController.swift
+//  PV239 project
+//
+//  Created by Jan Crha on 27/03/2019.
+//  Copyright © 2019 Jan Crha. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedUITabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: - UITabBarControllerDelegate
+extension AnimatedUITabBarController: UITabBarControllerDelegate  {
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let tabViewControllers = tabBarController.viewControllers!
+        guard let toIndex = tabViewControllers.index(of: viewController) else {
+            return false
+        }
+        animateToTab(toIndex: toIndex)
+        return true
+    }
+    
+    func animateToTab(toIndex: Int) {
+        guard let tabViewControllers = viewControllers else { return }
+        guard let selectedViewController = selectedViewController else { return }
+        guard let fromView = selectedViewController.view else { return }
+        guard let toView = tabViewControllers[toIndex].view else { return }
+        guard let fromIndex = tabViewControllers.index(of: selectedViewController) else { return }
+        
+        guard fromIndex != toIndex else { return }
+        
+        // Add the toView to the tab bar view
+        fromView.superview!.addSubview(toView)
+        
+        // Position toView off screen (to the left/right of fromView)
+        let screenWidth = UIScreen.main.bounds.size.width;
+        let scrollRight = toIndex > fromIndex;
+        let offset = (scrollRight ? screenWidth : -screenWidth)
+        toView.center = CGPoint(x: fromView.center.x + offset, y: toView.center.y)
+        
+        // Disable interaction during animation
+        view.isUserInteractionEnabled = false
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            
+            // Slide the views by -offset
+            fromView.center = CGPoint(x: fromView.center.x - offset, y: fromView.center.y);
+            toView.center   = CGPoint(x: toView.center.x - offset, y: toView.center.y);
+            
+        }, completion: { finished in
+            
+            // Remove the old view from the tabbar view.
+            fromView.removeFromSuperview()
+            self.selectedIndex = toIndex
+            self.view.isUserInteractionEnabled = true
+        })
+    }
+}
